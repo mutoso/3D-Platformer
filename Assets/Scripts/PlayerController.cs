4f501150e29d932ef649936712e5af599eb37862
Alastair Feille <me@alastair.se>
2024-04-14 06:46:48 -0500

    Fix double jumping bug

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody rb;
     [SerializeField] float speed = 50;
     [SerializeField] float jumpForce = 10;
+    [SerializeField] float groundCheckDistance = 1;
     [SerializeField] Animator anim;
 
+    Rigidbody rb;
     Vector2 movementInput;
     Vector3 movementVector;
+    bool onGround = false;
 
     // Start is called before the first frame update
     void Start()
@@ -21,11 +23,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        onGround = Physics.Raycast(transform.position, Vector3.down, groundCheckDistance);
+
         float horizontalAxis = Input.GetAxis("Horizontal");
         float verticalAxis = Input.GetAxis("Vertical");
         movementInput = new Vector2(horizontalAxis, verticalAxis);
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && onGround)
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode.Impulse);
             anim.SetTrigger("jump");
