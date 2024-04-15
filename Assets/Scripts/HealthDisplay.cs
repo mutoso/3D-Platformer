dec57436a28d2bae89f99fe660a5054ea8faf39e
Alastair Feille <me@alastair.se>
2024-04-14 19:31:30 -0500

    Add score and health UI

--- Assets/Scripts/HealthDisplay.cs
+++ Assets/Scripts/HealthDisplay.cs
@@ -3,19 +3,19 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class ScoreDisplay : MonoBehaviour
+public class HealthDisplay : MonoBehaviour
 {
     [SerializeField] PlayerStats playerStats;
 
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.GetComponent<TMP_Text>().text = "Score: " + playerStats.score;
+        gameObject.GetComponent<TMP_Text>().text = "Health: " + playerStats.health;
     }
 
     // Update is called once per frame
     void Update()
     {
-        gameObject.GetComponent<TMP_Text>().text = "Score: " + playerStats.score;
+        gameObject.GetComponent<TMP_Text>().text = "Health: " + playerStats.health;
     }
 }
