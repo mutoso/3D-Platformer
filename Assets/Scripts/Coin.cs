aeeb20d6b673a351d9a62c508b98bf0d84fc0d6a
Alastair Feille <me@alastair.se>
2024-04-14 19:46:07 -0500

    Make coins rotate

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -13,7 +13,7 @@ public class Coin : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        gameObject.transform.Rotate(new Vector3((100 * Time.deltaTime) % 360, 0, 0));
     }
 
     private void OnTriggerEnter(Collider other)
